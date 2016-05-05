e6e38239a14b287703037f5582a731eb94d65cc6
Paul Worlton <pworlton@hotmail.com>
2016-05-05 09:49:37 -0600

    Update snippets.php

--- snippets.php
+++ snippets.php
@@ -3,3 +3,17 @@
 // Open a file
 Route::get('file/{id}', 'HomeController@getFile');
 
+/**
+ * Open file
+ */
+public function getFile($filename)
+{
+    // Full path
+    $path = storage_path('app/' . $filename);
+
+    // Determine the mime type
+    $mimetype = mime_content_type($path);
+
+    // Return the file
+    return response()->file($path, ['Content-Type: ' . $mimetype . '; charset=utf-8']);
+}
