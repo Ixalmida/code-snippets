3248a6889d35bd6245b950ddcf388fcd1f799c44
Paul Worlton <pworlton@hotmail.com>
2016-05-18 08:32:27 -0600

    Update WidgetController.php

--- WidgetController.php
+++ WidgetController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Widget;
+use Illuminate\Http\Request;
 
 class WidgetController extends Controller
 {
-    //
+    /**
+     * List widgets
+     */
+    public function listWidgets()
+    {
+        // Return an array of widgets
+        return Widget::get()->toArray();
+    }
 }
