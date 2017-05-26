86504b6561983831990203b875db050cab48b949
Paul Worlton <pworlton@hotmail.com>
2017-05-26 11:37:10 -0600

    Initial commit.

---
+++ example1.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<script>
+  var subtypes1 = ["Blue", "Red"];
+  var subtypes2 = ["Yellow", "Green"];
+
+  function toggleSubtype(obj) {
+    console.log('toggling...');
+    var type = $(obj).find('option:selected').val();
+    var sub = $(obj).closest('.type_div').find('.subtype_id');
+
+    // Remove current subtypes
+    $(sub).empty();
+
+    // Populate matching subtypes
+    if (type === "1") {
+      $.each(subtypes1, function (i, val) {
+        $(sub).append($("<option></option>").attr("value", val).text(val));
+      });
+    } else {
+      $.each(subtypes2, function (i, val) {
+        $(sub).append($("<option></option>").attr("value", val).text(val));
+      });
+    }
+  }
+</script>
+</head>
+
+<body>
+  <div class="type_div">
+      Widget Type:
+      <select name="type_id[]" onchange="toggleSubtype(this)">
+          <option></option>
+          <option value="1">Consumer</option>
+          <option value="2">Business</option>
+      </select>
+
+      Widget Subtype:
+      <select name="subtype_id[]" class="subtype_id">
+      </select>
+  </div>
+</body>
+</html>
